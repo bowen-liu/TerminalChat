80c8453ec52613e564fbb94b4eda572a211290b2
Bowen <bin4ry@live.com>
2018-06-19 13:10:00 -0700

    Server and Client now compiles into seperate executables

--- main.c
+++ main.c
@@ -1,51 +1,56 @@
 #include "common/common.h"
 
-extern void server(const char* hostname, const unsigned int port);
-extern void client(const char* hostname, const unsigned int port, char *username);
 
+#if defined(SERVER_BUILD) && !defined(CLIENT_BUILD)
 
-int main(int argc, char *argv[])
-{
-    //TODO: use getopt
-    char ipaddr[INET_ADDRSTRLEN];
-    unsigned int port = 0;
+    extern void server(const char* hostname, const unsigned int port);
+
+    int main(int argc, char *argv[])
+    {
+        //TODO: use getopt
+        char ipaddr[INET_ADDRSTRLEN];
+        unsigned int port = 0;
     
-    if(argc < 2)
-    {
-        printf("Not enough args...\n");
-        return 0;
-    }
-
-    //Server
-    if(strcmp(argv[1], "-s") == 0)
-    {
-        if(argc < 3)
+        if(argc < 2)
+        {
+            printf("Server usage: chatserver <ip>:<port>\n");
+            printf("Binding to INADDR_ANY on default port...\n");
             server(NULL, DEFAULT_SERVER_PORT);
-       
+        }
         else
         {
-            sscanf(argv[2], "%[^:]:%u", ipaddr, &port);
+            sscanf(argv[1], "%[^:]:%u", ipaddr, &port);
             if(port == 0)
                 port = DEFAULT_SERVER_PORT;
             
             server(ipaddr, port);
-        }
-    }  
+        } 
+    }
 
-    //Client
-    else
+#elif defined(CLIENT_BUILD) && !defined(SERVER_BUILD)
+
+    extern void client(const char* hostname, const unsigned int port, char *username);
+
+    int main(int argc, char *argv[])
     {
-        if(argc < 4)
+        //TODO: use getopt
+        char ipaddr[INET_ADDRSTRLEN];
+        unsigned int port = 0;
+        
+        if(argc < 3)
         {
-            printf("Not enough args for client...\n");
+            printf("Client usage: chatclient <desired_username> <server_ip>:<server_port>\n");
             return 0;
         }
         
-        sscanf(argv[3], "%[^:]:%u", ipaddr, &port);
+        sscanf(argv[2], "%[^:]:%u", ipaddr, &port);
         if(port == 0)
                 port = DEFAULT_SERVER_PORT;
 
-        client(ipaddr, port, argv[2]);
+        client(ipaddr, port, argv[1]);
+            
     }
-        
-}
+
+#else
+    #error "You must define one of SERVER_BUILD or CLIENT_BUILD!"
+#endif
